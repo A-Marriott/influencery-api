dfbd21e7ab1a088dc03cf1d6041529d36009af6f
Alex Marriott <marriottaog@gmail.com>
2021-06-11 12:01:16 +0100

    added validations to models

--- app/models/influencer_tag.rb
+++ app/models/influencer_tag.rb
@@ -1,4 +1,6 @@
 class InfluencerTag < ApplicationRecord
   belongs_to :influencer
   belongs_to :tag
+
+  validates :influencer_id, uniqueness: { scope: :tag_id }
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
     has_many :influencer_tags
     has_many :influencers, through: :influencer_tags
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
